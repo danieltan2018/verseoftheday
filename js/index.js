ca3afde77d41ba67a5019269b03ea0dfe9eaaabb
danieltan2018 <50669678+danieltan2018@users.noreply.github.com>
2020-11-24 21:38:24 +0800

    Revert "Force update verse"
    This reverts commit 53c2a2f6ccb4a3e80ccd85c4ed8578ce80ef5437.

--- js/index.js
+++ js/index.js
@@ -36,7 +36,9 @@ function getVerse(setVerse) {
             el.innerHTML = json.html;
             window.localStorage.setItem('bibleText', el.getElementsByClassName("dailyVerses")[0].innerHTML);
             window.localStorage.setItem('bibleVerse', el.getElementsByClassName("dailyVerses")[1].getElementsByTagName("a")[0].innerHTML);
-            initVerse();
+            if (setVerse) {
+                initVerse();
+            }
         }
     });
 };
