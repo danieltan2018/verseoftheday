2c8fe52536f9deecebc5e7676fca68fa58f59351
Daniel Tan <50669678+danieltan2018@users.noreply.github.com>
2020-12-25 17:23:39 +0800

    Update unsplash source

--- js/index.js
+++ js/index.js
@@ -42,7 +42,7 @@ function getVerse() {
 
 function getPhoto(setPhoto) {
     var imgxhr = new XMLHttpRequest();
-    imgxhr.open("GET", "https://source.unsplash.com/1920x1080/?nature,background");
+    imgxhr.open("GET", "https://source.unsplash.com/2560x1440/?landscape,background");
     imgxhr.responseType = "blob";
     imgxhr.onload = function () {
         if (imgxhr.status === 200) {
